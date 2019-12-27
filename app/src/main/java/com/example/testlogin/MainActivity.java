62bed734819f13fbed6a7fbcd326974567517c9a
Franco <francocastillo2@hotmail.com>
2019-12-26 20:32:48 -0600

    pDF gENERATION

--- app/src/main/java/com/example/testlogin/MainActivity.java
+++ app/src/main/java/com/example/testlogin/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.testlogin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.api2pdf.client.Api2PdfClient;
+import com.api2pdf.models.Api2PdfResponse;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -17,6 +22,9 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+
+
+import java.io.IOException;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
         parameters.putString(FACEBOOK_FIELDS, FACEBOOK_FIELD_PROFILE_IMAGE);
         request.setParameters(parameters);
         request.executeAsync();
+    }
+
+    private void updateAvatar(String imageUrl) {
+
     }
 
     private static String FACEBOOK_FIELD_PICTURE = "picture";
@@ -94,6 +106,29 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
         }
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Api2PdfClient a2pClient = new Api2PdfClient("2677ef5d-022c-470b-ae4c-72cddf2fe81e");
+
+                        Api2PdfResponse pdfResponse = null;
+                        try {
+                            pdfResponse = a2pClient.headlessChromeFromHtml("<p>test</p>", true, "test.pdf");
+                            Log.e("PDF",pdfResponse.getPdf());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/testlogin/TestLogin.java
@@ -0,0 +1,11 @@
+package com.example.testlogin;
+
+import android.app.Application;
+
+public class TestLogin extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //MultiDex.install(this);
+    }
+}
